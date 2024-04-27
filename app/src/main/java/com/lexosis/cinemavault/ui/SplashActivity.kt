2aa97d31c858d79ac77f6b52cad11170fb64b8c5
JereCe <jeremiascejas93@gmail.com>
2024-04-27 18:54:29 -0300

    splash

---
+++ app/src/main/java/com/lexosis/cinemavault/ui/SplashActivity.kt
@@ -0,0 +1,47 @@
+package com.lexosis.cinemavault.ui
+
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.lexosis.cinemavault.R
+import com.bumptech.glide.Glide
+
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+            showGIF()
+
+            Handler(Looper.getMainLooper()).postDelayed({
+                var intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }, 4000)
+
+        }
+
+
+        private fun showGIF(){
+            val imageView: ImageView = findViewById(R.id.imagenViewPop)
+            Glide.with(this).load(R.drawable.popcorn).into(imageView)
+
+            }
+
+
+}
+
